d995ae30635f645a1656b3076c76ab0ab7d5dedd
MUS7ANG <salihjudo@icloud.com>
2024-07-17 11:07:37 +0600

    Initial commit

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyDJwDGCjYA9nk1EHaJAlT7EYIQ5VM4PfIo',
+      appId: '1:99407959619:ios:4fed433d35cd72eefe66f6',
+      messagingSenderId: '99407959619',
+      projectId: 'winged-quanta-403613',
+      storageBucket: 'winged-quanta-403613.appspot.com',
+    );
+  }
+}
